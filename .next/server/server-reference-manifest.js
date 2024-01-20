b182ce63c94988cc842151d3a5d210a5b5658be7
shoma5689 <shomaaslanov@gmail.com>
2024-01-20 18:05:00 +0300

    first commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NPdMxj4aMElJaVf+42krxlRUSIVK4S0pNFUz+THkBuk=\"\n}"
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Invoicers Page</p>
+    </div>
+  );
+}
